d8bd2811bca4c71d17e0629f6fa9f07cb8dca2db
João Felipe <felipewgt56@gmail.com>
2020-08-14 18:05:04 -0300

    Criei um codigo Questao02_Letra_C.py

--- Questao02_Letra_B2.py
+++ Questao02_Letra_B2.py
@@ -1,12 +1,13 @@
 def media (a, b, c):
     return a, b, c
 
+
 def main():
     a = int(input())
     b = int(input())
     c = int(input())
-    media = (a + b + c)/ 3
-    print(f'{media:.1f}')
-
+    medi = (a + b + c)/ 3
+    med = media(a, b, c)
+    print(f'{medi}')
 if __name__== '__main__':
     main()
